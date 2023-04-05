46688d989f1b032d56422401028553ee65516101
albernalca <albernalca@gmail.com>
2023-04-04 19:30:18 -0500

    Se declaran variables en el ts componente convertidor, en el html se realizan databinding con las variables creadas

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-convertidor',
   templateUrl: './convertidor.component.html',
   styleUrls: ['./convertidor.component.css']
 })
-export class ConvertidorComponent {
+export class ConvertidorComponent implements OnInit {
 
+  //declaro variables
   textoInput= 'Digite información';
+  cantidad = 0;
+  tengo = 'USD'
+  quiero = 'EUR'
+  total = 0;
+  
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
